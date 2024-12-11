184d481c60c817595e2b12a4c449efdf5bb49096
Harsh <harshjha8453@gmail.com>
2024-12-11 14:03:02 +0530

    feat: complete delete issue function and button
    - Finalized the implementation of the delete issue button
    - Connected the button with the delete issue function
    - Ensured functionality is working correctly in the UI

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,30 +1,43 @@
 "use client"
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+    const router = useRouter()
     return (
         <AlertDialog.Root>
             <AlertDialog.Trigger>
-        <Button color='red'>
-            Delete Issue
-        </Button>
-        </AlertDialog.Trigger>
-        <AlertDialog.Content>
-            <AlertDialog.Title>
-                Confirm Deletion
-            </AlertDialog.Title>
-            <AlertDialog.Description>
-                Are You Sure you want to delte this issue?This action cannot be undone.
-            </AlertDialog.Description>
-            <Flex mt="4" gap='4'>
-                <AlertDialog.Cancel>
-                    <Button color='gray' variant='soft'>Cancle</Button>
-                </AlertDialog.Cancel>
-                <AlertDialog.Action>
-                    <Button color='red'>Delete Issue</Button>
-                </AlertDialog.Action>
-            </Flex>
-        </AlertDialog.Content>
+                <Button color='red'>
+                    Delete Issue
+                </Button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content>
+                <AlertDialog.Title>
+                    Confirm Deletion
+                </AlertDialog.Title>
+                <AlertDialog.Description>
+                    Are You Sure you want to delte this issue?This action cannot be undone.
+                </AlertDialog.Description>
+                <Flex mt="4" gap='4'>
+                    <AlertDialog.Cancel>
+                        <Button color='gray' variant='soft'>Cancle</Button>
+                    </AlertDialog.Cancel>
+                    <AlertDialog.Action>
+                        <Button color='red' onClick={async () => {
+                            try {
+                                await axios.delete(`/api/issues/${issueId}`);
+                                router.push('/issues');
+                                router.refresh();
+                            } catch (error) {
+                                console.log('Error deleting issue:', error);
+                            }
+                        }}>
+                            Delete Issue
+                        </Button>
+                    </AlertDialog.Action>
+                </Flex>
+            </AlertDialog.Content>
 
         </AlertDialog.Root>
     )
