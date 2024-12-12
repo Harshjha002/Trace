649ac2e50928da6a942750a10d156b91ab1095e2
Harsh <harshjha8453@gmail.com>
2024-12-12 17:30:37 +0530

    feat: implement Google OAuth and update user models
    - Added Google OAuth functionality in auth.js
    - Updated models for users, accounts, sessions, and verification tokens
    - Integrated authentication flow with enhanced user model handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,33 @@
 
-export default function Home() {
+import { auth, signOut,signIn } from "@/auth"
+import { Button } from "@radix-ui/themes"
+
+export default async function Home() {
+  const session = await auth()
   return (
     <>
     <h1>Hello world</h1>
+    <div>
+      {session && session.user ? (<>
+      <form action={async () =>{ 
+        'use server'
+        await signOut({redirectTo:'/'})
+        
+        }}>
+        <Button type="submit">
+        Sing out
+        </Button>
+      </form></>) : (
+        <form action={async () => {
+        "use server"
+        await signIn('google')
+        }}>
+        <Button type="submit">
+          sign in
+        </Button>
+      </form>)}
+    </div>
+
     </>
   )
 }
